37140824cf0855518c0d329af6e28337f184dc7d
Valerio Vaccaro <valerio.vaccaro@gmail.com>
2025-03-02 15:18:44 +0100

    fixes

--- docker.sh
+++ docker.sh
@@ -4,7 +4,7 @@ for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do
 
 # Add Docker's official GPG key:
 sudo apt-get update
-sudo apt-get install ca-certificates curl
+sudo apt-get install -y ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -16,4 +16,4 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose
--- node.sh
+++ node.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 sudo apt update
-sudo apt install -y node npm
+sudo apt install -y nodejs npm
 sudo npm i -g node-red opentimestamps pm2
