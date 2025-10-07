174753c7fef801187f8cf78f55022e8beb15b936
Valerio Vaccaro <valerio.vaccaro@gmail.com>
2025-10-07 15:38:52 +0200

    update sparrow

--- sparrow.sh
+++ sparrow.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-wget https://github.com/sparrowwallet/sparrow/releases/download/2.0.0/sparrow_2.0.0-1_amd64.deb
-wget https://github.com/sparrowwallet/sparrow/releases/download/2.0.0/sparrow-2.0.0-manifest.txt
-wget https://github.com/sparrowwallet/sparrow/releases/download/2.0.0/sparrow-2.0.0-manifest.txt.asc
+wget https://github.com/sparrowwallet/sparrow/releases/download/2.2.3/sparrowwallet_2.2.3-1_amd64.deb
+wget https://github.com/sparrowwallet/sparrow/releases/download/2.2.3/sparrow-2.2.3-manifest.txt
+wget https://github.com/sparrowwallet/sparrow/releases/download/2.2.3/sparrow-2.2.3-manifest.txt.asc
 
-sudo dpkg -i sparrow_2.1.3-1_amd64.deb
+sudo dpkg -i sparrowwallet_2.2.3-1_amd64.deb
