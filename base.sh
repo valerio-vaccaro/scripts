721d205e73a5c17199a26e66dc310c4932ae5c1d
Valerio Vaccaro <valerio.vaccaro@gmail.com>
2025-10-07 15:38:28 +0200

    fix for debian 13

--- base.sh
+++ base.sh
@@ -2,4 +2,4 @@
 
 sudo apt update
 sudo apt upgrade -y
-sudo apt install -y mc vim curl wget wput build-essential nload ncdu htop btop lolcat fortunes polygen dadadodo polygen hollywood terminator tor
+sudo apt install -y mc vim curl wget wput build-essential nload ncdu htop btop lolcat fortunes polygen dadadodo* polygen hollywood terminator tor
