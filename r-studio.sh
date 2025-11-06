1c17314eddd10dd0f8399e6e4829dbb2f308437b
Valerio Vaccaro <valerio.vaccaro@gmail.com>
2025-11-06 14:28:56 +0100

    update scripts and permisions

--- r-studio.sh
+++ r-studio.sh
@@ -3,5 +3,5 @@
 sudo apt install r-base r-base-dev
 sudo apt install libssl-dev libclang-dev
 
-wget https://download1.rstudio.org/electron/jammy/amd64/rstudio-2025.05.1-513-amd64.deb
-sudo dpkg -i rstudio-2025.05.1-513-amd64.deb
+wget https://download1.rstudio.org/electron/jammy/amd64/rstudio-2025.09.2-418-amd64.deb
+sudo dpkg -i rstudio-2025.09.2-418-amd64.deb
--- sparrow.sh
+++ sparrow.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-wget https://github.com/sparrowwallet/sparrow/releases/download/2.3.0/sparrowwallet_2.3.0-1_amd64.deb
-wget https://github.com/sparrowwallet/sparrow/releases/download/2.3.0/sparrowserver_2.3.0-1_amd64.deb
-wget https://github.com/sparrowwallet/sparrow/releases/download/2.3.0/sparrow-2.3.0-manifest.txt
-wget https://github.com/sparrowwallet/sparrow/releases/download/2.3.0/sparrow-2.3.0-manifest.txt.asc
+wget https://github.com/sparrowwallet/sparrow/releases/download/2.3.1/sparrowwallet_2.3.1-1_amd64.deb
+wget https://github.com/sparrowwallet/sparrow/releases/download/2.3.1/sparrowserver_2.3.1-1_amd64.deb
+wget https://github.com/sparrowwallet/sparrow/releases/download/2.3.1/sparrow-2.3.1-manifest.txt
+wget https://github.com/sparrowwallet/sparrow/releases/download/2.3.1/sparrow-2.3.1-manifest.txt.asc
 
-sudo dpkg -i sparrowwallet_2.3.0-1_amd64.deb
-sudo dpkg -i sparrowserver_2.3.0-1_amd64.deb
+sudo dpkg -i sparrowwallet_2.3.1-1_amd64.deb
+sudo dpkg -i sparrowserver_2.3.1-1_amd64.deb
 
