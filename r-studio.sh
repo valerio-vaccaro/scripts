3f745708240a8096e454a0a541aec2da94544b10
Valerio Vaccaro <valerio.vaccaro@gmail.com>
2025-11-17 10:42:24 +0100

    update rstudio

--- r-studio.sh
+++ r-studio.sh
@@ -5,3 +5,5 @@ sudo apt install libssl-dev libclang-dev
 
 wget https://download1.rstudio.org/electron/jammy/amd64/rstudio-2025.09.2-418-amd64.deb
 sudo dpkg -i rstudio-2025.09.2-418-amd64.deb
+
+rm rstudio-2025.09.2-418-amd64.deb
