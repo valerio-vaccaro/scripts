0ff7f39cdc2a4f7d5a872f495af5c7e5f88f784c
Valerio Vaccaro <valerio.vaccaro@gmail.com>
2025-11-17 10:43:04 +0100

    update to 2.3.1

--- sparrow.sh
+++ sparrow.sh
@@ -8,3 +8,5 @@ wget https://github.com/sparrowwallet/sparrow/releases/download/2.3.1/sparrow-2.
 sudo dpkg -i sparrowwallet_2.3.1-1_amd64.deb
 sudo dpkg -i sparrowserver_2.3.1-1_amd64.deb
 
+rm sparrowwallet_2.3.1-1_amd64.deb
+rm sparrowserver_2.3.1-1_amd64.deb
