e64a6ca30ec7c213b6b261c63ce719c94d4163a4
Valerio Vaccaro <valerio.vaccaro@gmail.com>
2025-11-28 18:21:52 +0100

    use version in rstudio

--- r-studio.sh
+++ r-studio.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+VERSION=rstudio-2025.09.2-418-amd64.deb
+
 sudo apt install r-base r-base-dev
 sudo apt install libssl-dev libclang-dev
 
-wget https://download1.rstudio.org/electron/jammy/amd64/rstudio-2025.09.2-418-amd64.deb
-sudo dpkg -i rstudio-2025.09.2-418-amd64.deb
+wget https://download1.rstudio.org/electron/jammy/amd64/$VERSION
+sudo dpkg -i $VERSION
 
-rm rstudio-2025.09.2-418-amd64.deb
+rm $VERSION
