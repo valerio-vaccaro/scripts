20ffca7fbcd8c398d4a9fae5a2d0388cfa68c888
Valerio Vaccaro <valerio.vaccaro@gmail.com>
2026-02-14 22:13:54 +0100

    add tailscale

---
+++ tailscale.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Add Tailscale's GPG key
+sudo mkdir -p --mode=0755 /usr/share/keyrings
+curl -fsSL https://pkgs.tailscale.com/stable/debian/trixie.noarmor.gpg | sudo tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
+# Add the tailscale repository
+curl -fsSL https://pkgs.tailscale.com/stable/debian/trixie.tailscale-keyring.list | sudo tee /etc/apt/sources.list.d/tailscale.list
+# Install Tailscale
+sudo apt-get update && sudo apt-get install tailscale
+# Start Tailscale!
+sudo tailscale up
