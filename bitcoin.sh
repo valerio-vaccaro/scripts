7bbc15a1d27d15c99c0120bf4f32c689b525db14
Valerio Vaccaro <valerio.vaccaro@gmail.com>
2026-02-14 21:16:33 +0100

    update bitcoin

--- bitcoin.sh
+++ bitcoin.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-VERSION=30.0
+VERSION=30.2
 
 wget https://bitcoincore.org/bin/bitcoin-core-$VERSION/bitcoin-$VERSION-x86_64-linux-gnu.tar.gz
 wget https://bitcoincore.org/bin/bitcoin-core-$VERSION/SHA256SUMS
